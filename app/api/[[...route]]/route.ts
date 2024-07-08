07841b9fd1f460055eead811d8348d8cce87c6f7
jppctech <dilipmorigaon@gmail.com>
2024-07-08 19:31:38 +0530

    header component

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,15 @@
+import { handle } from "hono/vercel";
+import { Hono } from "hono";
+
+export const runtime = "edge"
+
+const app = new Hono().basePath("/api")
+
+app.get("/hello", (c) => {
+    return c.json({
+        hello: "hello world"
+    })
+})
+
+export const GET = handle(app)
+export const POST = handle(app)
